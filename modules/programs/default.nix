9725853c87309cf85c1f264fd27288634d8a5b5e
Yuchen He <yuchenhe126@gmail.com>
2024-09-28 16:30:14 +0200

    programs/desktop.nix: Init default desktop application module
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./desktop.nix
     ./gaming.nix
   ];
 }
---
+++ modules/programs/desktop.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.myModules.programs.desktop;
+in
+{
+  options = {
+    myModules.programs.desktop = {
+      enable = mkEnableOption "default desktop applications" // {
+        default = config.myModules.desktop.enable;
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      ## Password manager
+      keepassxc
+
+      ## Network
+      networkmanagerapplet
+
+      ## Communication
+      mumble
+      discord
+      element-desktop
+
+      ## Other
+      thunderbird # email client
+      xournalpp
+      spotify
+      parsec-bin # remote desktop client
+      obs-studio
+      obsidian # note taking
+      jellyfin-media-player
+    ];
+  };
+}
