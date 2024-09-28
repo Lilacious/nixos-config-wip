18fe5990809b99cc130f89cd49a6a7fd3f4aea37
Yuchen He <yuchenhe126@gmail.com>
2024-09-28 12:55:22 +0200

    printing.nix: Init printer module
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

---
+++ modules/services/printing.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.myModules.services.printing;
+in
+{
+  options = {
+    myModules.services.printing = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    ## Printer with avahi
+    services.printing.enable = true;
+    services.avahi.enable = true;
+    services.avahi.nssmdns4 = true;
+    ## For a WiFi printer
+    services.avahi.openFirewall = mkDefault true;
+  };
+}
