3693a72ef7d5a91a4565fc7b397c2ba2c3dc2995
Yuchen He <yuchenhe126@gmail.com>
2024-11-01 17:14:43 +0100

    core/eza.nix: Fix deprecated icon config
    Signed-off-by: Yuchen He <yuchenhe126@gmail.com>

--- home/core/eza.nix
+++ home/core/eza.nix
@@ -20,7 +20,7 @@ in
     programs = {
       eza = {
         enable = true;
-        icons = true;
+        icons = "auto";
         extraOptions = [
           "--group"
           "--group-directories-first"
